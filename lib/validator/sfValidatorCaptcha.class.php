8f3999ca26be6de7dc41d92dd8a10e1598bb9622
tomi <tomi@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-09-23 08:21:34 -0500

    [sfPEARcaptchaPlugin] captcha validator
    git-svn-id: http://svn.symfony-project.com/plugins/sfPEARcaptchaPlugin/trunk@33078 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/validator/sfValidatorCaptcha.class.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Description of sfValidatorCaptcha
+ *
+ * @author Tomasz Jakub Rup
+ */
+class sfValidatorCaptcha extends sfValidatorBase {
+
+	/**
+	 * Configures the current validator.
+	 *
+	 * @param array $options   An array of options
+	 * @param array $messages  An array of error messages
+	 *
+	 * @see sfValidatorBase
+	 */
+	protected function configure($options = array(), $messages = array()) {
+		$this->setMessage('invalid', '"%value%" is a bad answer.');
+	}
+
+	/**
+	 * @see sfValidatorBase
+	 */
+	public function doClean($value) {
+		$phrase = sfContext::getInstance()->getStorage()->read('captcha/phrase');
+
+		if ($phrase != $value) {
+			throw new sfValidatorError($this, 'invalid', array('value' => $value));
+		}
+
+		return $value;
+	}
+
+}
